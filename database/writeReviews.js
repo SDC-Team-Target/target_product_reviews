6282d87b65a4930554af1337614ab98d5284d498
Danielle Blom <dlblom91@gmail.com>
2020-08-29 11:42:34 -0500

    Generate 10M fake reviews

---
+++ database/writeReviews.js
@@ -0,0 +1,41 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const writeReviews = fs.createWriteStream('./reviewData.csv');
+writeReviews.write('reviewID,customerName,reviewTitle,reviewText,productID,rating,date\n', 'utf8');
+
+const writeTenMillionReviews = (writer, encoding, callback) => {
+  let i = 10000000;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const reviewID = faker.random.number({ min: 1, max: 10000000 });
+      const customerName = faker.internet.userName();
+      const reviewTitle = faker.commerce.productAdjective();
+      const reviewText = faker.lorem.sentence();
+      const productID = faker.random.number({ min: 1, max: 10000000 });
+      const rating = faker.random.number({ min: 1, max: 5 })
+      const date = faker.date.recent();
+
+      const data = `${reviewID},${customerName},${reviewTitle},${reviewText},${productID},${rating},${date}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+        if (i > 0) {
+        // write some more once it drains
+        writer.once('drain', write);
+    }
+  }
+  write()
+}
+
+writeTenMillionReviews(writeReviews, 'utf-8', () => {
+  writeReviews.end();
+});
+
